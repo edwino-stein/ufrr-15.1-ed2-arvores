ccbfe65fec23a00ecfcc2b1a4b9da04156c625e5
Edwino Stein <edwino.stein@gmail.com>
2015-05-12 00:30:30 -0400

    Documentando árvore B

--- Tree/header/BTree.hpp
+++ Tree/header/BTree.hpp
@@ -7,53 +7,209 @@
 namespace tree{
 
 	/**
-	 * @todo: Documentar
+	 * @brief Árvore B para números inteiros.
 	 * 
-	 * @ver B Mark IV
+	 * @version B Mark IV
 	 */
-
 	class BTree : public Tree{
 
 	protected:
 
+		/**
+		 * @brief Ordem (m) da árvore.
+		 */
 		const unsigned int order;
+
+		/**
+		 * @brief Número maximo de chaves que uma página pode ter (2*m).
+		 */
 		const unsigned int maxKeys;
+
+		/**
+		 * @brief Número maximo de filhos que uma página pode ter (2*m + 1).
+		 */
 		const unsigned int maxBranches;
 
+		/**
+		 * @brief Referência para raiz da árvore
+		 */
 		BTreePage *root;
 
+		/**
+		 * @brief Insere um valor na árvore
+		 * @details Método recursivo da inserção, pois percorre toda a árvore procurando uma folha que o valor pode ser inserido.
+		 * A inserção de uma nova chave deve respeitar as regras de uma árvore B:
+		 * <ul>
+		 * 	<li>Valores menores ficam a esquerda;</li>
+		 * 	<li>Valores maiores ficam a direita;</li>
+		 * 	<li>Não se permite valores repetidos;</li>
+		 * 	<li>As páginas não podem ter mais que 2*m chaves;</li>
+		 * 	<li>As páginas não podem ter menos que m chaves;</li>
+		 * 	<li>A raiz pode ter no mínimo 1 chave;</li>
+		 * </ul> 
+		 * 
+		 * @param value Valor que será inserido.
+		 * @param page Referência da página pai.
+		 * 
+		 * @return <b>TRUE</b> caso o valor tenha sido inserido, e <b>FALSE</b> para caso o valor já exista na árvore.
+		 */
 		bool insert(int value, BTreePage *page);
 
+		/**
+		 * @brief Procura e caso ache, remove o valor da árvore.
+		 * @details Método recursivo que realiza a remoção de uma chave de uma página da árvore.
+		 * 	
+		 * @param value Valor que será procurado e removido.
+		 * @param page Referência da página pai.
+		 * 
+		 * @return <b>TRUE</b> caso o valor tenha sido removido, e <b>FALSE</b> para caso não tenha encontrado o valor.
+		 */
 		bool remove(int value, BTreePage *page);
 
+		/**
+		 * @brief Procura um valor na árvore.
+		 * @details Método recursivo que realiza uma busca de uma chave da árvore.
+		 * 
+		 * @param value Valor que será procurado.
+		 * @param history Vetor contendo os valores dos nós consultados.
+		 * @param page Referência da página pai.
+		 * 
+		 * @return <b>TRUE</b> caso o valor tenha sido encontrado, e <b>FALSE</b> para caso não tenha encontrado o valor.
+		 */
 		bool search(int value, collection& history, BTreePage *page);
 
+		/**
+		 * @brief Busca o valor do sucessor de um valor da página.
+		 * @details Retorna o primeiro valor da página mais a esquerda da página da direita do valor.
+		 * 
+		 * @param value Valor da página.
+		 * @param page Referência da página.
+		 * @return Valor do sucessor.
+		 */
 		int getSucessor(int value, BTreePage *page);
 
+		/**
+		 * @brief Redistribui chaves entre páginas.
+		 * @details Método responsavel por realizar distribuição entre páginas para manter o equilíbrio da árvore após uma remoção.
+		 * 
+		 * @param unsigned int Índice da página filho que está desequilibrada.
+		 * @param page Referência da página pai.
+		 */
 		void redistribute(unsigned int index, BTreePage *page);
 
 	public:
 
+		/**
+		 * @brief Contrutor padrão.
+		 * @details Cria uma árvore vazia.
+		 * 
+		 * @param unsigned int Ordem (m) da árvore.
+		 */
 		BTree(unsigned int order);
 
+		/**
+		 * @brief Contrutor de inicialização.
+		 * @details Cria uma árvore inicializando com um valor.
+		 * 
+		 * @param unsigned int Ordem (m) da árvore.
+		 * @param value Valor da raiz.
+		 */
 		BTree(unsigned int order, int value);
 
+		/**
+		 * @brief Destrutor padrão
+		 */
 		~BTree();
 
+		/**
+		 * @brief Insere um valor na árvore
+		 * @details Método público da inserção, chama o método recusivo passando a raiz como parâmetro.
+		 * A inserção de uma nova chave deve respeitar as regras de uma árvore B:
+		 * <ul>
+		 * 	<li>Valores menores ficam a esquerda;</li>
+		 * 	<li>Valores maiores ficam a direita;</li>
+		 * 	<li>Não se permite valores repetidos;</li>
+		 * 	<li>As páginas não podem ter mais que 2*m chaves;</li>
+		 * 	<li>As páginas não podem ter menos que m chaves;</li>
+		 * 	<li>A raiz pode ter no mínimo 1 chave;</li>
+		 * </ul> 
+		 * 
+		 * @param value Valor que será inserido.
+		 * 
+		 * @return <b>TRUE</b> caso o valor tenha sido inserido, e <b>FALSE</b> para caso o valor já exista na árvore.
+		 */
 		BTree *insert(int value);
 
+		/**
+		 * @brief Procura e caso ache, remove o valor da árvore.
+		 * @details Método público que realiza a remoção de uma chave de uma página da árvore.
+		 * 	
+		 * @param value Valor que será procurado e removido.
+		 * 
+		 * @return <b>TRUE</b> caso o valor tenha sido removido, e <b>FALSE</b> para caso não tenha encontrado o valor.
+		 */
 		bool remove(int value);
 
+		/**
+		 * @brief Procura um valor na árvore.
+		 * @details Método público que realiza uma busca de uma chave da árvore.
+		 * 
+		 * @param value Valor que será procurado.
+		 * @param history Vetor contendo os valores consultados.
+		 * 
+		 * @return <b>TRUE</b> caso o valor tenha sido encontrado, e <b>FALSE</b> para caso não tenha encontrado o valor.
+		 */
 		bool search(int value, collection& history);
 
+		/**
+		 * @brief Percorre cada elemento da árvore.
+		 * @details Percorre a árvore toda pela ordem desejada, passando o valor de cada página para a função anônima definida.
+		 * A função anônima deve ser no formado <b>void (int)</b>. Exemplo de código:
+		 * @code
+		 * 	BTree arvore;
+		 * 
+		 * 	//...
+		 *
+		 * 	int soma = 0;
+		 * 
+		 * 	arvore.each([&soma](int valorDoNo){
+		 * 		soma += valorDoNo;
+		 * 	});
+		 * 
+		 * @endcode
+		 * 
+		 * @param handle Função anônima que será executada para cada chave.
+		 * @param order Tipo de ordem que a a árvore será percorrida:
+		 * <ul>
+		 * 	<li><b>tree::RED_ORDER</b>: Pré-Ordem (Raiz-Esquerda-Direita)</li>
+		 * 	<li><b>tree::ERD_ORDER</b>: Em-Ordem (Esquerda-Raiz-Direita)</li>
+		 * 	<li><b>tree::EDR_ORDER</b>: Pós-Ordem (Esquerda-Direita-Raiz)</li>
+		 * </ul>
+		 */
 		void each(std::function<void(int)> handle, char order = tree::RED_ORDER);
 
+		/**
+		 * @brief Verifica árvore está vazia.
+		 * @return TRUE para vazio, FALSE para não vazio.
+		 */
 		bool isEmpty();
 
+		/**
+		 * @brief Método que libera os recuros da árvore e subárvores.
+		 * @details Libera os recuros e limpa todos as páginas da árvore, deixando-a vazia.
+		 */
 		void clear();
 
+		/**
+		 * @brief Calcula a altura da árvore.
+		 * @details Calcula e retorna a altura da árvore.
+		 * @return Altura da árvore.
+		 */
 		int height();
 
+		/**
+		 * @brief Percorre página por página imprimindo todas as chaves da árvore.
+		 */
 		void print();
 
 	};
--- Tree/header/BTreePage.hpp
+++ Tree/header/BTreePage.hpp
@@ -6,63 +6,186 @@
 
 namespace tree{
 
+	/**
+	 * @brief Página da árvore B
+	 */
 	class BTreePage : public Object{
 
 		friend class BTree;
 
 	protected:
 
+		/**
+		 * @brief Chaves da página
+		 */
 		std::vector<int> keys;
+
+		/**
+		 * @brief Filhos da página
+		 */
 		std::vector<BTreePage *> branches;
 
+		/**
+		 * @brief Destrutor padrão
+		 */
 		~BTreePage();
 
+		/**
+		 * @brief Insere uma chave em um local especifico da página.
+		 * 
+		 * @param value Valor da chave que será inserida.
+		 * @param unsigned int Índice da posição em que a cahve será inserida.
+		 * @param before Flag que indentidica se a chave será inserida antes ou depois do índice fornecido.
+		 */
 		void insertKey(int value, unsigned int index, bool before = true);
 
+		/**
+		 * @brief Adiciona uma chave ordenadamente na página.
+		 * @details Procura a posição ordenada e insere a chave.
+		 * 
+		 * @param value Valor da chave que será adicionada.
+		 */
 		void addKey(int value);
 
+		/**
+		 * @brief Remove uma chave da página.
+		 * @details Remove a chave presente na posição informada.
+		 * 
+		 * @param unsigned int Índice da posição da que chave será removida.
+		 * @return <b>TRUE</b> caso a chave tenha sido removida, e <b>FALSE</b> para caso a chave não exista na página.
+		 */
 		bool removeKey(unsigned int index);
 
+		/**
+		 * @brief Insere um filho em um local especifico da página.
+		 * 
+		 * @param unsigned int Índice da posição em que o filho será inserida.
+		 * @param page Referência da página. 
+		 * @param before Flag que indentidica se a página será inserida antes ou depois do índice fornecido.
+		 */
 		void insertBranche(unsigned int index, BTreePage *page, bool before = true);
 
+		/**
+		 * @brief Remove um filho da página.
+		 * @details Remove o filho presente na posição informada.
+		 * 
+		 * @param unsigned int Índice da posição do filho que será removida.
+		 * @return <b>TRUE</b> caso o filho tenha sido removido, e <b>FALSE</b> para caso o filho não exista na página.
+		 */		
 		bool removeBranche(unsigned int index, bool autoFree = true);
 
+		/**
+		 * @brief Percorre a página e seus filhos em Pré-Ordem.
+		 * @param handle Função anônima que será executada para cada chave.
+		 */
 		void eachPreOrder(std::function<void(int)> handle);
 
+		/**
+		 * @brief Percorre a página e seus filhos Em-Ordem.
+		 * @param handle Função anônima que será executada para cada chave.
+		 */
 		void eachInOrder(std::function<void(int)> handle);
 
+		/**
+		 * @brief Percorre a página e seus filhos em Pós-Ordem.
+		 * @param handle Função anônima que será executada para cada chave.
+		 */
 		void eachPostOrder(std::function<void(int)> handle);
 
-
+		/**
+		 * @brief Pega o índice do valor o do seu sucessor na página.
+		 * @details Busca a chave na página e retorna seu índice, caso não a encontre, retorna o índice do seu sucessor.
+		 * 
+		 * @param valor Valor da chave que será procurada.
+		 * @return Índice da chave na página.
+		 */
 		unsigned int getIndex(int valor);
 
+		/**
+		 * @brief Retorna o valor de uma chave da página.
+		 * 
+		 * @param unsigned int Índice da posição de que chave.
+		 * @return Valor da chave.
+		 */
 		int getKey(unsigned int index);
 
+		/**
+		 * @brief Retorna a referência de um filho da página.
+		 * 
+		 * @param unsigned int Índice da posição do filho.
+		 * @return Referência da página filho.
+		 */
 		BTreePage *getBranche(unsigned int index);
 
-
+		/**
+		 * @brief Define o valor de uma chave da página.
+		 * 
+		 * @param unsigned int Índice da posição de que chave.
+		 */
 		void setKey(unsigned int index, int value);
 
+		/**
+		 * @brief Define a referência de um filho da página.
+		 * 
+		 * @param unsigned int Índice da posição do filho.
+		 */
 		void setBranche(unsigned int index, BTreePage *page);
 
-
+		/**
+		 * @brief Verifica se a chave existe na página.
+		 * 
+		 * @param value Valor da chave que será buscada.
+		 * @return <b>TRUE</b> caso a chave tenha sido encontrada, e <b>FALSE</b> para caso a chave não exista na página.
+		 */
 		bool hasKey(int value);
 
+		/**
+		 * @brief Verifica se a página está vazio.
+		 * @return TRUE para vazio, FALSE para não vazio.
+		 */
 		bool isEmpty();
 
+		/**
+		 * @brief Verifica se a página é uma folha.
+		 * @details Verifica se a página não tem filhos.
+		 * @return TRUE se for folha, FALSE se tiver filhos.
+		 */
 		bool isLeaf();
 
-
+		/**
+		 * @brief Método que libera os recuros da página e filhos.
+		 * @details Libera os recuros e limpa todas as chaves página e seus filhos, deixando-a vazia.
+		 */
 		void clear();
 
+		/**
+		 * @brief Conta quantas chaves existem na página.
+		 * @return Total de chaves.
+		 */
 		unsigned int countKeys();
 
+		/**
+		 * @brief Conta quantos filhos existem na página.
+		 * @return Total de filhos.
+		 */
 		unsigned int countBranches();
 		
+		/**
+		 * @brief Divide a página em duas sub-páginas, onde na página quebrada resta apenas a chave pivô.
+		 * @param unsigned int Índice da posição da chave que será o pivô.
+		 */
 		void split(unsigned int pivot);
 
+		/**
+		 * @brief Imprime a lista de chaves da página.
+		 */
 		void printList();
 
+		/**
+		 * @brief Percorre chave por chave imprimindo toda a página e suas filhas.
+		 * @param page Referência da página.
+		 * @param spaces Quantidades de espaços para recuo para direita.
+		 */
 		static void printPage(BTreePage *page, int spaces);
 
 	};
